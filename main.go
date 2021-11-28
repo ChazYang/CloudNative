9cd3c390e5ecab7410d6efb606de8a424135e9e8
ChazYang <yangcz95@126.com>
2021-11-28 20:41:10 +0800

    fixed_5

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-28 20:05:06
- * @LastEditTime: 2021-11-28 20:06:12
+ * @LastEditTime: 2021-11-28 20:40:53
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \CloudNative\main.go
@@ -11,6 +11,6 @@
  
  import "fmt"
 
- func mian() {
+ func main() {
 	 fmt.Println("hello, cloud")
  }
