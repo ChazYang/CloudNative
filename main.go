df1709ef43a45dd3305418da3e6fe7469bfeb39a
ChazYang <yangcz95@126.com>
2021-11-30 01:27:48 +0800

    update wercker.yml

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 /*
  * @Author: your name
  * @Date: 2021-11-28 20:05:06
- * @LastEditTime: 2021-11-28 20:40:53
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-11-30 01:13:16
+ * @LastEditors: your name
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \CloudNative\main.go
  */
-
  package main
  
  import "fmt"
