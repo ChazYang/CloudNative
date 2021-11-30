9dfc5c22e84668ad838ce12e3dbf5105eb297aaa
ChazYang <yangcz95@126.com>
2021-12-01 00:11:30 +0800

    xiecheng

--- main.go
+++ main.go
@@ -1,15 +1,46 @@
-/*
- * @Author: your name
- * @Date: 2021-11-28 20:05:06
- * @LastEditTime: 2021-11-30 01:13:16
- * @LastEditors: your name
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: \CloudNative\main.go
- */
- package main
- 
- import "fmt"
+package main
 
- func main() {
-	 fmt.Println("hello, cloud")
- }
+import (
+	"strconv"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan string)
+
+	go pump1(ch1)
+	go pump2(ch2)
+	go suck(ch1, ch2)
+
+	time.Sleep(time.Duration(time.Second * 40))
+}
+
+func pump1(ch chan int)  {
+	for i := 0; ; i++ {
+		ch <- i * 2
+		time.Sleep(time.Duration(time.Second))
+	}
+}
+
+func pump2(ch chan string)  {
+	for i := 0; ; i++ {
+		ch <- strconv.Itoa(i + 5)
+		time.Sleep(time.Duration(time.Second))
+	}
+}
+
+func suck(ch1 chan int, ch2 chan string)  {
+	chRate := time.Tick(time.Duration(time.Second * 5))
+	for {
+		select {
+		case v := <-ch1:
+			fmt.Printf("Received on channel 1: %d\n", v)
+		case v := <-ch2:
+			fmt.Printf("Received on channel 2: %s\n", v)
+		case <- chRate:
+			fmt.Printf("Log log...\n")
+		}
+	}
+}
